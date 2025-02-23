493aec7e6c5e88637e67297cd746c1eb84648c39
UTOXAS <abdullah621@outlook.com>
2025-02-23 19:41:52 +0200

    Remove unused files and configurations from the project

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-light py-3 text-center">
-            <p className="mb-0">© 2025 Storefront. All rights reserved.</p>
-        </footer>
-    )
-}
-
-export default Footer;
